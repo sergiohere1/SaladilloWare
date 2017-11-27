a94e08098e0247bae5101681c16d13962a1829da
sergiohere1 <sergio11garcia@hotmail.com>
2017-11-27 09:40:12 +0100

    Add project files.

---
+++ Tienda/Tienda/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Tienda
+{
+    public partial class App : Application
+    {
+        public App(string filename)
+        {
+            InitializeComponent();
+            MainPage = new Tienda.MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ Tienda/Tienda/Tables/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace Tienda.Tables
+{
+    [Table("user")]
+    public class User
+    {
+        [PrimaryKey, AutoIncrement]
+        public int IdUser { }
+    }
+}
